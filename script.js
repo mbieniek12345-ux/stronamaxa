b34065956a83c8b9ffc4c34fa3cb7b1d0fa42806
stronamaxa <m.bieniek12345@gmail.com>
2026-02-01 18:20:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -106,3 +106,16 @@ function aktualizujZegarek() {
 // odpalamy zegarek co sekundę
 setInterval(aktualizujZegarek, 1000);
 aktualizujZegarek(); // od razu pokazuje czas
+let klikow = 0;
+const zdjecie = document.getElementById("mojeZdjecie");
+const egg = document.getElementById("easterEgg");
+
+zdjecie.addEventListener("click", () => {
+    klikow++;
+    if (klikow === 5) {
+        egg.style.display = "block";  // pokaż easter egg
+    } else if (klikow > 5) {          
+        egg.style.display = "none";    // schowaj
+        klikow = 0;                     // reset licznika
+    }
+});
